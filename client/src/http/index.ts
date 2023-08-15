a2274388bef12960810c6533779f817e0a73c936
Graff Vishenka <maxvisnh@gmail.com>
2023-08-15 17:22:14 +0500

    Finished client without css

---
+++ client/src/http/index.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { API_URL } from "../utils/consts";
+import { AuthResponse } from "../models/response/AuthResponse";
+
+const $api = axios.create({
+  withCredentials: true,
+  baseURL: API_URL,
+});
+
+$api.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  return config;
+});
+
+$api.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401) {
+      try {
+        const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {
+          withCredentials: true,
+        });
+        localStorage.setItem("token", response.data.accessToken);
+        return $api.request(originalRequest);
+      } catch (e) {
+        console.log("Unauthorized");
+      }
+    }
+  }
+);
+
+export default $api;
